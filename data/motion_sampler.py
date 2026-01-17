c6d0d734928b4fbed7fab7c7d6551a8e045dfd9c
maegonz <manutonymanu@gmail.com>
2026-01-17 18:07:05 +0100

    Remove debug prints

--- data/motion_sampler.py
+++ data/motion_sampler.py
@@ -22,7 +22,6 @@ class MotionSampler(Sampler):
         self.batch_size = batch_size
         self.bucket_size = bucket_size
         self.shuffle = shuffle
-        # max_frames =150
 
         # Group indices by frames
         buckets = defaultdict(list)
@@ -30,11 +29,6 @@ class MotionSampler(Sampler):
             bucket_id = frame // self.bucket_size
             buckets[bucket_id].append(id)
         
-        # print(f"Number of buckets created: {len(buckets)}")
-        # print(f"Bucket ids: {sorted(buckets.keys())}")
-        # print(f"Bucket sizes: {[len(v) for v in buckets.values()]}")
-        # print(f"Bucket examples: {list(buckets.values())[:5]}")
-
         # Create batches
         self.batches = []
         for bucket in buckets.values():
@@ -42,9 +36,6 @@ class MotionSampler(Sampler):
                 batch = bucket[i:i + self.batch_size]
                 self.batches.append(batch)
         
-        # print(f"Total number of batches created: {len(self.batches)}")
-        # print(f"Batch sizes: {[len(b) for b in self.batches]}")
-
         if self.shuffle:
             rd.shuffle(self.batches)
 
@@ -53,46 +44,3 @@ class MotionSampler(Sampler):
     
     def __iter__(self):
         yield from self.batches
-
-# import random as rd
-# example = MotionSampler(frames=[rd.randint(10, 150) for _ in range(146)], batch_size=16, shuffle=True)
-
-# class MotionSampler(Sampler):
-#     def __init__(
-#         self,
-#         frames,
-#         batch_size,
-#         bucket_size=32,
-#         max_frames=150,
-#         shuffle=True
-#     ):
-#         self.batch_size = batch_size
-#         self.shuffle = shuffle
-
-#         # Clip long sequences (IMPORTANT)
-#         frames = [min(f, max_frames) for f in frames]
-
-#         # Bucket by length ranges
-#         buckets = defaultdict(list)
-#         for idx, f in enumerate(frames):
-#             bucket_id = f // bucket_size
-#             buckets[bucket_id].append(idx)
-
-#         self.batches = []
-#         for bucket in buckets.values():
-#             if shuffle:
-#                 random.shuffle(bucket)
-#             for i in range(0, len(bucket), batch_size):
-#                 batch = bucket[i:i + batch_size]
-#                 if len(batch) == batch_size:
-#                     self.batches.append(batch)
-
-#         if shuffle:
-#             random.shuffle(self.batches)
-
-#     def __len__(self):
-#         return len(self.batches)
-
-#     def __iter__(self):
-#         yield from self.batches
-  
